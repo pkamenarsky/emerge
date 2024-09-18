5a626b28729cb884483699f5bbe28df7170c67cd
Philip Kamenarsky <p.kamenarsky@gmail.com>
2024-09-18 16:34:33 +0300

    lmao if this works

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,25 +1,26 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
 module Lib where
 
 import Control.Concurrent
-import Control.Concurrent.Chan
 
 import Control.Monad.Free
-import Control.Monad.IO.Class
 
 import Data.IORef
+import Data.Foldable (for_)
 
-data ViewId
-data ViewIndex
+--------------------------------------------------------------------------------
 
--- events alter the structure
--- signals govern shader uniforms
+data Event a = Event (IORef (Maybe a))
 
-data Event a = Event (IO (Maybe a))
+newEvent :: IO (Event a)
+newEvent = Event <$> newIORef Nothing
+
+--------------------------------------------------------------------------------
 
 data SynF v next
   = View v next
@@ -30,7 +31,7 @@ data SynF v next
 
 deriving instance Functor (SynF v)
 
-newtype Syn v a = Syn { syn :: Free (SynF v) a }
+newtype Syn v a = Syn (Free (SynF v) a)
   deriving (Functor, Applicative, Monad)
 
 forever :: Syn v a
@@ -59,15 +60,17 @@ data SynR v a
   | S                    -- stop
 
 unblock :: Monoid v => Syn v a -> IO (SynR v a)
-unblock (Syn (Pure a)) = pure $ P a
-unblock s@(Syn (Free (On (Event ref) next))) = do
-  a <- ref
-  case a of
-    Nothing -> pure $ B $ unblock s
-    Just a' -> unblock $ Syn $ next a'
-unblock (Syn (Free (View v next))) = pure $ V v (unblock $ Syn next)
+
+unblock (Syn (Pure a))       = pure $ P a
 unblock (Syn (Free Forever)) = pure S
-unblock s@(Syn (Free (Or a b next))) = go (unblock a) (unblock b) mempty mempty
+
+unblock s@(Syn (Free (On (Event ref) next))) = readIORef ref >>= \case
+  Just r  -> unblock $ Syn $ next r
+  Nothing -> pure $ B $ unblock s
+
+unblock (Syn (Free (View v next))) = pure $ V v (unblock $ Syn next)
+
+unblock (Syn (Free (Or a b next))) = go (unblock a) (unblock b) mempty mempty
   where
     go aSyn bSyn aPrV bPrV = do
       a' <- aSyn
@@ -93,8 +96,78 @@ unblock s@(Syn (Free (Or a b next))) = go (unblock a) (unblock b) mempty mempty
         (B aNext, B bNext) -> pure $ B $ go aNext bNext aPrV bPrV
 
         -- both stopped
-        (S, S)             -> pure S
+        (S, S) -> pure S
 
         -- one stopped
-        (S, _)             -> pure $ B $ unblock s
-        (_, S)             -> pure $ B $ unblock s
+        (S, B bNext) -> pure $ B $ go (pure S) bNext aPrV bPrV
+        (B aNext, S) -> pure $ B $ go aNext (pure S) aPrV bPrV
+
+unblock (Syn (Free (And a b next))) = go (unblock a) (unblock b) mempty mempty
+  where
+    go aSyn bSyn aPrV bPrV = do
+      a' <- aSyn
+      b' <- bSyn
+
+      case (a', b') of
+        -- both finished
+        (P aR, P bR) -> pure $ V mempty (unblock $ Syn $ next (aR <> bR))
+
+        -- both views
+        (V aV aNext, V bV bNext) -> pure $ V (aV <> bV) (go aNext bNext aV bV)
+
+        -- left view, remaining variants
+        (V aV aNext, B bNext) -> pure $ V (aV <> bPrV) (go aNext bNext aV bPrV)
+        (V aV aNext, p@(P _)) -> pure $ V (aV <> bPrV) (go aNext (pure p) aV bPrV)
+        (V aV aNext, S)       -> pure $ V (aV <> bPrV) (go aNext (pure S) aV bPrV)
+
+        -- right view, remaining variants
+        (B aNext, V bV bNext)  -> pure $ V (aPrV <> bV) (go aNext bNext aPrV bV)
+        (p@(P _), V bV bNext)  -> pure $ V (aPrV <> bV) (go (pure p) bNext aPrV bV)
+        (S, V bV bNext)        -> pure $ V (aPrV <> bV) (go (pure S) bNext aPrV bV)
+
+        -- both blocked
+        (B aNext, B bNext) -> pure $ B $ go aNext bNext aPrV bPrV
+
+        -- one blocked, the other finished
+        (B aNext, p@(P _)) -> pure $ B $ go aNext (pure p) aPrV bPrV
+        (p@(P _), B bNext) -> pure $ B $ go (pure p) bNext aPrV bPrV
+
+        -- both stopped
+        (S, S) -> pure S
+
+        -- one stopped, the other finished
+        (S, P _) -> pure S
+        (P _, S) -> pure S
+
+        -- one stopped, the other blocked
+        (S, B bNext) -> pure $ B $ go (pure S) bNext aPrV bPrV
+        (B aNext, S) -> pure $ B $ go aNext (pure S) aPrV bPrV
+
+--------------------------------------------------------------------------------
+
+run :: Monoid v => Syn v () -> (v -> IO ()) -> IO (Event a -> a -> IO ())
+run syn showView = do
+  synR <- unblock syn
+
+  mvSyn <- newMVar =<< case synR of
+    V v synRIO -> showView v >> pure synRIO
+    B synRIO   -> pure synRIO
+    S          -> error "stopped"
+    P ()       -> error "finished"
+
+  pure $ \(Event ref) a -> do
+    v <- modifyMVar mvSyn $ \synRIO -> do
+      writeIORef ref (Just a)
+
+      synR' <- synRIO
+
+      synRIO' <- case synR' of
+        V v synRIO' -> pure (synRIO', Just v)
+        B synRIO'   -> pure (synRIO', Nothing)
+        S           -> error "stopped"
+        P ()        -> error "finished"
+
+      writeIORef ref Nothing
+      pure synRIO'
+
+    for_ v showView
