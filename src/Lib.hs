1972d4cf4dc208703f0dd0ab1d12c59ced560aab
Philip Kamenarsky <p.kamenarsky@gmail.com>
2024-09-18 15:54:04 +0300

    wip

--- src/Lib.hs
+++ src/Lib.hs
@@ -25,7 +25,7 @@ data SynF v next
   = View v next
   | forall a. On (Event a) (a -> next)
   | forall a. Or (Syn v a) (Syn v a) (a -> next)
-  | forall a. And (Syn v a) (Syn v a) (a -> next)
+  | forall a. Monoid a => And (Syn v a) (Syn v a) (a -> next)
   | Forever
 
 deriving instance Functor (SynF v)
@@ -47,34 +47,54 @@ on e = Syn $ liftF $ On e id
 or :: Syn v a -> Syn v a -> Syn v a
 or a b = Syn $ liftF $ Or a b id
 
-and :: Syn v a -> Syn v a -> Syn v a
+and :: Monoid a => Syn v a -> Syn v a -> Syn v a
 and a b = Syn $ liftF $ And a b id
 
 --------------------------------------------------------------------------------
 
 data SynR v a
-  = P a            -- pure
-  | V v (Syn v a)  -- view
-  | B (Syn v a)    -- blocked
-  | S              -- stop
+  = P a                  -- pure
+  | V v (IO (SynR v a))  -- view
+  | B (IO (SynR v a))    -- blocked
+  | S                    -- stop
 
 unblock :: Monoid v => Syn v a -> IO (SynR v a)
 unblock (Syn (Pure a)) = pure $ P a
 unblock s@(Syn (Free (On (Event ref) next))) = do
   a <- ref
   case a of
-    Nothing -> pure $ B s
+    Nothing -> pure $ B $ unblock s
     Just a' -> unblock $ Syn $ next a'
-unblock (Syn (Free (Or a b next))) = do
-  a' <- unblock a
-  b' <- unblock b
-  case (a', b') of
-    (P r, _) -> unblock $ do
-      _ <- view mempty
-      Syn $ next r
-    (_, P r) -> unblock $ do
-      _ <- view mempty
-      Syn $ next r
-    (V av' an, V bv' bn) -> pure $ V (av' <> bv') (Syn $ Free $ Or an bn next)
-    (V av' an, B bn)   -> pure $ V (av' <> undefined) (Syn $ Free $ Or an bn next)
-      
+unblock (Syn (Free (View v next))) = pure $ V v (unblock $ Syn next)
+unblock (Syn (Free Forever)) = pure S
+unblock s@(Syn (Free (Or a b next))) = go (unblock a) (unblock b) mempty mempty
+  where
+    go aSyn bSyn aPrV bPrV = do
+      a' <- aSyn
+      b' <- bSyn
+
+      case (a', b') of
+        -- one finished
+        (P r, _) -> pure $ V mempty (unblock $ Syn $ next r)
+        (_, P r) -> pure $ V mempty (unblock $ Syn $ next r)
+
+        -- both views
+        (V aV aNext, V bV bNext) -> pure $ V (aV <> bV) (go aNext bNext aV bV)
+
+        -- left view, remaining variants
+        (V aV aNext, B bNext) -> pure $ V (aV <> bPrV) (go aNext bNext aV bPrV)
+        (V aV aNext, S)       -> pure $ V (aV <> bPrV) (go aNext (pure S) aV bPrV)
+
+        -- right view, remaining variants
+        (B aNext, V bV bNext)  -> pure $ V (aPrV <> bV) (go aNext bNext aPrV bV)
+        (S, V bV bNext)        -> pure $ V (aPrV <> bV) (go (pure S) bNext aPrV bV)
+
+        -- both blocked
+        (B aNext, B bNext) -> pure $ B $ go aNext bNext aPrV bPrV
+
+        -- both stopped
+        (S, S)             -> pure S
+
+        -- one stopped
+        (S, _)             -> pure $ B $ unblock s
+        (_, S)             -> pure $ B $ unblock s
