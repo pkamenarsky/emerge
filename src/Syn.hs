c2da1db58f244400a16d363cee8782e8701a1ae2
Philip Kamenarsky <philip@chordify.net>
2024-09-18 18:11:53 +0300

    specs

--- src/Syn.hs
+++ src/Syn.hs
@@ -12,9 +12,7 @@ import Control.Concurrent
 import Control.Monad.Free
 
 import Data.IORef
-import Data.Foldable (asum, for_)
-import qualified Data.List.NonEmpty as NE
-import Data.Semigroup
+import Data.Foldable (for_)
 
 --------------------------------------------------------------------------------
 
@@ -166,53 +164,3 @@ run syn showView = do
       pure synRIO'
 
     for_ v showView
-
---------------------------------------------------------------------------------
-
-test :: IO ()
-test = do
-  ref <- newIORef ""
-  e <- newEvent :: IO (Event ())
-  fire <- run (s1 e) (writeIORef ref)
-
-  fire e ()
-  cmp ref "AAC"
-  fire e ()
-  cmp ref "RRS"
-  fire e ()
-  cmp ref "BB"
-  fire e ()
-  cmp ref "C"
-
-  where
-    cmp ref should = do
-      is <- readIORef ref
-      if is == should
-        then putStrLn "OK"
-        else putStrLn $ "FAIL (" <> is <> " should be " <> should <> ")"
-
-    s1 e = do
-      pure ()
-      r <- sconcat $ NE.fromList [ asum [ view "A", view "A", view "C", on e >> pure "R" ], on e >> pure "R", on e >> pure "S" ]
-      _ <- asum [ view r, on e]
-      pure ()
-      pure ()
-      pure ()
-      asum
-        [ do
-            pure ()
-            pure ()
-            _ <- view "B"
-            pure ()
-        , do
-            pure ()
-            view "B"
-        , do
-            pure ()
-            _ <- on e
-            pure ()
-        ]
-      pure ()
-      pure ()
-      pure ()
-      asum [ view "C", on e ]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,55 @@
+import Syn
+
+import Data.IORef
+
+import Data.Foldable (asum)
+import qualified Data.List.NonEmpty as NE
+import Data.Semigroup
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  ref <- newIORef ""
+  e <- newEvent :: IO (Event ())
+  fire <- run (s1 e) (writeIORef ref)
+
+  fire e ()
+  cmp ref "AAC"
+  fire e ()
+  cmp ref "RRS"
+  fire e ()
+  cmp ref "BB"
+  fire e ()
+  cmp ref "C"
+
+  where
+    cmp ref should = do
+      is <- readIORef ref
+      if is == should
+        then putStrLn "OK"
+        else putStrLn $ "FAIL (" <> is <> " should be " <> should <> ")"
+
+    s1 e = do
+      pure ()
+      r <- sconcat $ NE.fromList [ asum [ view "A", view "A", view "C", on e >> pure "R" ], on e >> pure "R", on e >> pure "S" ]
+      _ <- asum [ view r, on e]
+      pure ()
+      pure ()
+      pure ()
+      asum
+        [ do
+            pure ()
+            pure ()
+            _ <- view "B"
+            pure ()
+        , do
+            pure ()
+            view "B"
+        , do
+            pure ()
+            _ <- on e
+            pure ()
+        ]
+      pure ()
+      pure ()
+      pure ()
+      asum [ view "C", on e ]
