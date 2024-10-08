13d3c5f11f0c677b727344300d84db374eb7d823
Philip Kamenarsky <philip@chordify.net>
2024-10-08 22:49:15 +0300

    gen

--- src/Gen.hs
+++ src/Gen.hs
@@ -1,33 +1,16 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedLabels #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
 
 module Gen where
 
 import Control.Monad.IO.Class
 import           Control.Monad.Trans.Class (lift)
 import           Control.Monad.Trans.Reader
-import qualified Control.Monad.Trans.State as ST
-import qualified Control.Monad.Trans.Writer.CPS as W
 
 import Data.Foldable (asum)
-import Data.String.Interpolate (i)
 import Data.Text (Text)
-import qualified Data.Text as T
-
-import qualified Graphics.Rendering.OpenGL as GL
 
 import Common
 import Syn
@@ -35,162 +18,16 @@ import Types
 
 import GHC.Generics
 
-data GenOp params = GenOp
-  { gopTex :: GL.TextureObject
-  , gopRender :: forall subparams. Params subparams => SubSet subparams params ~ 'True => HList subparams -> IO ()
-  , gopDestroy :: IO ()
-  }
 
-genShader
-  :: FromTuples tuples (HList params)
-  => Params params
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> tuples
-  -> (ParamFields params -> Text)
-  -> IO (GenOp params)
-genShader rectBuf opts deriveOpts tuples fragT = do
-  (tex, bindFBO, destroyFBO) <- createFramebuffer opts
-  let params = fromTuples tuples
-  let (udefs, initUniforms) = shaderParams' deriveOpts params
-  (attribs, bindShader, destroyShader) <- createShader Nothing (fragT udefs)
-
-  bindShader
-  uniforms <- initUniforms (saProgram attribs)
-
-  (drawRect, destroyDrawRect) <- createDrawRect rectBuf attribs
-
-  pure $ GenOp
-    { gopTex = tex
-    , gopRender = \params -> do
-        bindFBO
-        bindShader
-        set uniforms params
-        drawRect
-    , gopDestroy = do
-        destroyFBO
-        destroyShader
-        destroyDrawRect
-    }
-
-genShader1
-  :: FromTuples tuples (HList params)
-  => Params params
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> tuples
-  -> (ParamFields (Param "tex0" (Texture 0) ': params) -> Text)
-  -> IO (GenOp (Param "tex0" (Texture 0) ': params))
-genShader1 rectBuf opts deriveOpts tuples fragT = do
-  (tex, bindFBO, destroyFBO) <- createFramebuffer opts
-  let params = param #tex0 (texture @0 Nothing) :. fromTuples tuples
-  let (udefs, initUniforms) = shaderParams' deriveOpts params
-  (attribs, bindShader, destroyShader) <- createShader Nothing (fragT udefs)
-
-  bindShader
-  uniforms <- initUniforms (saProgram attribs)
-
-  (drawRect, destroyDrawRect) <- createDrawRect rectBuf attribs
-
-  pure $ GenOp
-    { gopTex = tex
-    , gopRender = \params -> do
-        bindFBO
-        bindShader
-        set uniforms params
-        drawRect
-    , gopDestroy = do
-        destroyFBO
-        destroyShader
-        destroyDrawRect
-    }
-
-genShader2
-  :: FromTuples tuples (HList params)
-  => Params params
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> tuples
-  -> (ParamFields (Param "tex0" (Texture 0) ': Param "tex1" (Texture 1) ': params) -> Text)
-  -> IO (GenOp (Param "tex0" (Texture 0) ': Param "tex1" (Texture 1) ': params))
-genShader2 rectBuf opts deriveOpts tuples fragT = do
-  (tex, bindFBO, destroyFBO) <- createFramebuffer opts
-  let params = param #tex0 (texture @0 Nothing) :. param #tex1 (texture @1 Nothing) :. fromTuples tuples
-  let (udefs, initUniforms) = shaderParams' deriveOpts params
-  (attribs, bindShader, destroyShader) <- createShader Nothing (fragT udefs)
-
-  bindShader
-  uniforms <- initUniforms (saProgram attribs)
-
-  (drawRect, destroyDrawRect) <- createDrawRect rectBuf attribs
-
-  pure $ GenOp
-    { gopTex = tex
-    , gopRender = \params -> do
-        bindFBO
-        bindShader
-        set uniforms params
-        drawRect
-    , gopDestroy = do
-        destroyFBO
-        destroyShader
-        destroyDrawRect
-    }
-
-data GenSignal params = forall subtuples subparams subsigparams. (FromTuples subtuples (HList subsigparams), SeqParams subsigparams subparams, Params subparams, SubSet subparams params ~ 'True) => GenSignal subtuples
-
-genShaderSyn''
-  :: MonadIO m
-  => ShaderParams params
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> (ParamFields params -> Text)
-  -> params
-  -> Syn [Out] m a
-genShaderSyn'' rectBuf opts deriveOpts fragT params = do
-  (out, destroy) <- unsafeNonBlockingIO $ do
-    (tex, bindFBO, destroyFBO) <- createFramebuffer opts
-    let (fields, initUniforms) = shaderParams deriveOpts
-    (attribs, bindShader, destroyShader) <- createShader Nothing (fragT fields)
-
-    bindShader
-    setUniforms <- initUniforms (saProgram attribs)
-
-    (drawRect, destroyDrawRect) <- createDrawRect rectBuf attribs
-
-    pure
-      ( Out
-          { outTex = tex
-          , outRender = do
-              bindFBO
-              bindShader
-              setUniforms params
-              drawRect
-          }
-      , do
-          destroyFBO
-          destroyShader
-          destroyDrawRect
-      )
-
-  finalize (liftIO destroy) $ view [out]
-
-genShaderSyn'''
+shader0
   :: ShaderParams params
   => ShaderParamDeriveOpts
   -> (ParamFields params -> Text)
   -> params
   -> Op a
-genShaderSyn''' deriveOpts fragT params = Op $ do
+shader0 deriveOpts fragT params = Op $ do
   OpContext opts rectBuf <- lift ask
+
   (out, destroy) <- unsafeNonBlockingIO $ do
     (tex, bindFBO, destroyFBO) <- createFramebuffer opts
     let (fields, initUniforms) = shaderParams deriveOpts
@@ -221,18 +58,16 @@ genShaderSyn''' deriveOpts fragT params = Op $ do
 data Syn1 = Syn1 { tex0 :: Signal (Texture 0) }
   deriving Generic
 
-genShaderSyn1''
-  :: MonadIO m
-  => ShaderParams params
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
+shader1
+  :: ShaderParams params
+  => ShaderParamDeriveOpts
   -> (ParamFields params -> ParamFields Syn1 -> Text)
   -> params
-  -> Syn [Out] m a
-  -> Syn [Out] m a
-genShaderSyn1'' rectBuf opts deriveOpts fragT params syn = do
+  -> Op a
+  -> Op a
+shader1 deriveOpts fragT params op1 = Op $ do
+  OpContext opts rectBuf <- lift ask
+
   (f, destroy) <- unsafeNonBlockingIO $ do
     (tex, bindFBO, destroyFBO) <- createFramebuffer opts
 
@@ -267,24 +102,22 @@ genShaderSyn1'' rectBuf opts deriveOpts fragT params syn = do
           destroyDrawRect
       )
 
-  finalize (liftIO destroy) $ mapView (pure . f) syn
+  finalize (liftIO destroy) $ mapView (pure . f) (runOp op1)
 
 data Syn2 = Syn2 { tex0 :: Signal (Texture 0), tex1 :: Signal (Texture 1) }
   deriving Generic
 
-genShaderSyn2''
-  :: MonadIO m
-  => ShaderParams params
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
+shader2
+  :: ShaderParams params
+  => ShaderParamDeriveOpts
   -> (ParamFields params -> ParamFields Syn2 -> Text)
   -> params
-  -> Syn [Out] m a
-  -> Syn [Out] m a
-  -> Syn [Out] m a
-genShaderSyn2'' rectBuf opts deriveOpts fragT params syn1 syn2 = do
+  -> Op a
+  -> Op a
+  -> Op a
+shader2 deriveOpts fragT params op1 op2 = Op $ do
+  OpContext opts rectBuf <- lift ask
+
   (f, destroy) <- unsafeNonBlockingIO $ do
     (tex, bindFBO, destroyFBO) <- createFramebuffer opts
 
@@ -323,114 +156,4 @@ genShaderSyn2'' rectBuf opts deriveOpts fragT params syn1 syn2 = do
           destroyDrawRect
       )
 
-  finalize (liftIO destroy) $ mapView (pure . f) $ asum [ syn1, syn2 ]
-
-genShaderSyn
-  :: MonadIO m
-  => FromTuples tuples (HList params)
-  => Params params
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> tuples
-  -> (ParamFields params -> Text)
-  -> GenSignal params
-  -> Syn [Out] m a
-genShaderSyn rectBuf opts deriveOpts tuples fragT (GenSignal signal) = do
-  GenOp tex render destroy <- unsafeNonBlockingIO $ genShader rectBuf opts deriveOpts tuples fragT
-
-  finalize (liftIO destroy) $ view $ pure $ Out
-    { outTex = tex
-    , outRender = seqParams (fromTuples signal) >>= render
-    }
-
--- type SubParams params subtuples =
---   forall subparams subsigparams.
---   (  FromTuples subtuples (HList subsigparams)
---   => SeqParams subsigparams subparams
---   => Params subparams
---   => SubSet subparams params ~ 'True
---   )
-
-type SubParams params subtuples subparams subsigparams =
-    ( FromTuples subtuples (HList subsigparams)
-    , SeqParams subsigparams subparams
-    , Params subparams
-    , SubSet subparams params ~ 'True
-    )
-
-genShaderSyn'
-  :: MonadIO m
-  => FromTuples tuples (HList params)
-  => Params params
-
-  => FromTuples subtuples (HList subsigparams)
-  => SeqParams subsigparams subparams
-  => Params subparams
-  => SubSet subparams params ~ 'True
-
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> tuples
-  -> (ParamFields params -> Text)
-  -> subtuples
-  -> Syn [Out] m a
-genShaderSyn' rectBuf opts deriveOpts tuples fragT signal = do
-  GenOp tex render destroy <- unsafeNonBlockingIO $ genShader rectBuf opts deriveOpts tuples fragT
-
-  finalize (liftIO destroy) $ view $ pure $ Out
-    { outTex = tex
-    , outRender = seqParams (fromTuples signal) >>= render
-    }
-
-genShaderSyn1
-  :: MonadIO m
-  => FromTuples tuples (HList params)
-  => Params params
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> tuples
-  -> (ParamFields (Param "tex0" (Texture 0) ': params) -> Text)
-  -> GenSignal params
-  -> Syn [Out] m a
-  -> Syn [Out] m a
-genShaderSyn1 rectBuf opts deriveOpts tuples fragT (GenSignal signal) op0 = do
-  GenOp tex render destroy <- unsafeNonBlockingIO $ genShader1 rectBuf opts deriveOpts tuples fragT
-  finalize (liftIO destroy) $ mapView (pure . f tex render) op0
-  where
-    f tex render [out] = Out
-      { outTex = tex
-      , outRender = do
-          outRender out
-          (fmap (param #tex0 (texture @0 (Just $ outTex out)) :.) $ seqParams (fromTuples signal)) >>= render
-      }
-
-genShaderSyn2
-  :: MonadIO m
-  => FromTuples tuples (HList params)
-  => Params params
-  => RectBuffer
-  -> OpOptions
-  -> ShaderParamDeriveOpts
-  -> tuples
-  -> (ParamFields (Param "tex0" (Texture 0) ': Param "tex1" (Texture 1) ': params) -> Text)
-  -> GenSignal params
-  -> Syn [Out] m a
-  -> Syn [Out] m a
-  -> Syn [Out] m a
-genShaderSyn2 rectBuf opts deriveOpts tuples fragT (GenSignal signal) op0 op1 = do
-  GenOp tex render destroy <- unsafeNonBlockingIO $ genShader2 rectBuf opts deriveOpts tuples fragT
-  finalize (liftIO destroy) $ mapView (pure . f tex render) $ asum [ op0, op1 ]
-  where
-    g out1 out2 p = param #tex0 (texture @0 (Just $ outTex out1)) :. param #tex1 (texture @1 (Just $ outTex out2)) :. p
-
-    f tex render [out1, out2] = Out
-      { outTex = tex
-      , outRender = do
-          outRender out1
-          outRender out2
-          fmap (g out1 out2) (seqParams (fromTuples signal)) >>= render
-      }
+  finalize (liftIO destroy) $ mapView (pure . f) $ asum [ runOp op1, runOp op2 ]
